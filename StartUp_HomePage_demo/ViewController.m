639033ac6a36d68f61ac582f7411885f5e76d17a
Derek <derek@DerekdeMacBook-Pro.local>
2017-04-29 16:41:05 +0800

    Initial Commit

---
+++ StartUp_HomePage_demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StartUp_HomePage_demo
+//
+//  Created by Derek on 2017/4/29.
+//  Copyright © 2017年 www.ioslover.club. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
